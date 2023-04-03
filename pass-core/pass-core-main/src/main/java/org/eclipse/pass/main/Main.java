c445b422fbe0cff58df0bfe2c58819f2d613747f
Timothy Sanders <tsande16@jhu.edu>
2023-04-03 10:11:39 -0400

    rebase with main

--- pass-core/pass-core-main/src/main/java/org/eclipse/pass/main/Main.java
+++ pass-core/pass-core-main/src/main/java/org/eclipse/pass/main/Main.java
@@ -27,7 +27,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 @ComponentScan(basePackages = {"org.eclipse.pass", "org.eclipse.pass.doi.service",
-    "org.eclipse.pass.file.service", "org.eclipse.pass.user"})
+    "org.eclipse.pass.file.service", "org.eclipse.pass.user", "org.eclipse.pass.metadata.service"})
 @EntityScan(basePackages = { "org.eclipse.pass.object.model" })
 public class Main {
     /**
