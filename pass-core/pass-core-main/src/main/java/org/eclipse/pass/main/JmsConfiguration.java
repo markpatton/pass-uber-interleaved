fdeac35653fa3d93dc47634321e1f597659ba050
Mark Patton <mark.patton@gmail.com>
2023-06-01 16:19:12 -0400

    Add support for sending JMS messages on certain object changes to an ActiveMQ or a Amazon SQS broker

---
+++ pass-core/pass-core-main/src/main/java/org/eclipse/pass/main/JmsConfiguration.java
@@ -0,0 +1,213 @@
+package org.eclipse.pass.main;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Optional;
+import java.util.Set;
+import javax.jms.ConnectionFactory;
+import javax.json.Json;
+import javax.json.JsonObjectBuilder;
+
+import com.amazon.sqs.javamessaging.ProviderConfiguration;
+import com.amazon.sqs.javamessaging.SQSConnectionFactory;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.yahoo.elide.RefreshableElide;
+import com.yahoo.elide.annotation.LifeCycleHookBinding.Operation;
+import com.yahoo.elide.annotation.LifeCycleHookBinding.TransactionPhase;
+import com.yahoo.elide.core.dictionary.EntityDictionary;
+import com.yahoo.elide.core.dictionary.Injector;
+import com.yahoo.elide.core.lifecycle.LifeCycleHook;
+import com.yahoo.elide.core.type.Type;
+import com.yahoo.elide.core.utils.ClassScanner;
+import com.yahoo.elide.core.utils.coerce.CoerceUtil;
+import com.yahoo.elide.modelconfig.DynamicConfiguration;
+import com.yahoo.elide.spring.config.ElideConfigProperties;
+import org.eclipse.pass.object.model.Deposit;
+import org.eclipse.pass.object.model.EventType;
+import org.eclipse.pass.object.model.Submission;
+import org.eclipse.pass.object.model.SubmissionEvent;
+import org.eclipse.pass.usertoken.KeyGenerator;
+import org.eclipse.pass.usertoken.TokenFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.core.JmsTemplate;
+
+/**
+ * Configures Elide such that updates to Submission, SubmissionEvent, and Deposit send messages to a JMS broker.
+ */
+@Configuration
+public class JmsConfiguration {
+    private static final Logger LOG = LoggerFactory.getLogger(JmsConfiguration.class);
+
+    static final String APPROVAL_LINK_KEY = "approval-link";
+    static final String DEPOSIT_MESSAGE_TYPE = "DepositStatus";
+    static final String SUBMISSION_MESSAGE_TYPE = "SubmissionReady";
+    static final String SUBMISSION_KEY = "submission";
+    static final String SUBMISSION_EVENT_MESSAGE_TYPE = "SubmissionEvent";
+    static final String DEPOSIT_KEY = "deposit";
+    static final String SUBMISSION_EVENT_KEY = "submission-event";
+    static final String TYPE_KEY = "type";
+
+    @Value("${pass.jms.queue.submission}")
+    private String submission_queue;
+
+    @Value("${pass.jms.queue.submission-event}")
+    private String submission_event_queue;
+
+    @Value("${pass.jms.queue.deposit}")
+    private String deposit_queue;
+
+    /**
+     * @return name of queue for Submission object updates
+     */
+    String getSubmissionQueue() {
+        return submission_queue;
+    }
+
+    /**
+     * @return name of queue for SubmissionEvent object updates
+     */
+    String getSubmissionEventQueue() {
+        return submission_event_queue;
+    }
+
+    /**
+     * @return name of queue for Deposit object updates
+     */
+    String getDepositQueue() {
+        return deposit_queue;
+    }
+
+    /**
+     * Return TokenFactory configured with a key. Generate the key if it is not given.
+     *
+     * @param key token key or null
+     * @param refreshableElide a RefreshableElide instance
+     * @return TokenFactory
+     */
+    @Bean
+    public TokenFactory userTokenFactory(@Value("${pass.usertoken.key:#{null}}") String key,
+            RefreshableElide refreshableElide) {
+        if (key == null || key.isEmpty()) {
+            key = KeyGenerator.generateKey();
+
+            LOG.error("No user token key specified, generating key");
+        }
+
+        return new TokenFactory(key);
+    }
+
+    /**
+     * Provide a JMS connection to Amazon SQS if configured to do so.
+     *
+     * @param awsRegion AWS region
+     * @return JmsListenerContainerFactory
+     */
+    @Bean
+    @ConditionalOnProperty(name = "pass.jms.sqs", havingValue = "true", matchIfMissing = false)
+    public ConnectionFactory jmsConnectionFactory(@Value("${aws.region}") String awsRegion) {
+        return new SQSConnectionFactory(new ProviderConfiguration(),
+                AmazonSQSClientBuilder.standard().withRegion(Regions.fromName(awsRegion)));
+    }
+
+    /**
+     * Override the EnitityDictionary bean in order to add hooks.
+     *
+     * @param injector the Injector
+     * @param scanner the ClassScanner
+     * @param optionalDynamicConfig the DynamicConfiguration
+     * @param settings the ElideConfigProperties
+     * @param entitiesToExclude the set of entities to exclude
+     * @param jms the JmsTemplate used by the hooks
+     * @param userTokenFactory the TokenFactory
+     * @return configured EntityDictionary.
+     */
+    @Bean
+    public EntityDictionary buildDictionary(Injector injector, ClassScanner scanner,
+            Optional<DynamicConfiguration> optionalDynamicConfig, ElideConfigProperties settings,
+            @Qualifier("entitiesToExclude") Set<Type<?>> entitiesToExclude, JmsTemplate jms,
+            TokenFactory userTokenFactory) {
+
+        EntityDictionary dictionary = new EntityDictionary(new HashMap<>(), new HashMap<>(), injector,
+                CoerceUtil::lookup, entitiesToExclude, scanner);
+
+        setupHooks(dictionary, jms, userTokenFactory);
+
+        return dictionary;
+    }
+
+    private void setupHooks(EntityDictionary dictionary, JmsTemplate jms, TokenFactory userTokenFactory) {
+        LifeCycleHook<SubmissionEvent> sub_event_hook = (op, phase, event, scope, changes) -> {
+            send(jms, submission_event_queue, createMessage(event, userTokenFactory));
+        };
+
+        LifeCycleHook<Submission> sub_hook = (op, phase, sub, scope, changes) -> {
+            if (sub.getSubmitted() != null && sub.getSubmitted() == true) {
+                send(jms, submission_queue, createMessage(sub));
+            }
+        };
+
+        LifeCycleHook<Deposit> deposit_hook = (op, phase, dep, scope, changes) -> {
+            send(jms, deposit_queue, createMessage(dep));
+        };
+
+        dictionary.bindTrigger(SubmissionEvent.class, Operation.CREATE, TransactionPhase.POSTCOMMIT, sub_event_hook,
+                false);
+
+        dictionary.bindTrigger(Submission.class, Operation.CREATE, TransactionPhase.POSTCOMMIT, sub_hook, false);
+        dictionary.bindTrigger(Submission.class, Operation.UPDATE, TransactionPhase.POSTCOMMIT, sub_hook, false);
+
+        dictionary.bindTrigger(Deposit.class, Operation.CREATE, TransactionPhase.POSTCOMMIT, deposit_hook, false);
+        dictionary.bindTrigger(Deposit.class, Operation.UPDATE, TransactionPhase.POSTCOMMIT, deposit_hook, false);
+    }
+
+    private String createMessage(Submission s) {
+        return Json.createObjectBuilder().add(SUBMISSION_KEY, s.getId().toString()).add(TYPE_KEY,
+                SUBMISSION_MESSAGE_TYPE).build().toString();
+    }
+
+    private String getInvitationLink(SubmissionEvent ev, TokenFactory userTokenFactory) {
+        Submission sub = ev.getSubmission();
+
+        if (sub != null && sub.getSubmitterEmail() != null && ev.getLink() != null) {
+            URI uri = userTokenFactory.forPassResource("submission", sub.getId(), sub.getSubmitterEmail())
+                    .addTo(ev.getLink());
+
+            return uri.toString();
+        } else {
+            LOG.warn("Cannot create an invitation link, missing required information: " + sub);
+        }
+
+        return null;
+    }
+
+    private String createMessage(SubmissionEvent ev, TokenFactory userTokenFactory) {
+        JsonObjectBuilder ob = Json.createObjectBuilder().add(SUBMISSION_EVENT_KEY, ev.getId().toString()).add(TYPE_KEY,
+                SUBMISSION_EVENT_MESSAGE_TYPE);
+
+        if (ev.getEventType() == EventType.APPROVAL_REQUESTED_NEWUSER) {
+            String link = getInvitationLink(ev, userTokenFactory);
+
+            if (link != null) {
+                ob.add(APPROVAL_LINK_KEY, link);
+            }
+        }
+
+        return ob.build().toString();
+    }
+
+    private String createMessage(Deposit dep) {
+        return Json.createObjectBuilder().add(DEPOSIT_KEY, dep.getId().toString()).add(TYPE_KEY,
+                DEPOSIT_MESSAGE_TYPE).build().toString();
+    }
+
+    private void send(JmsTemplate jms, String queue, String text) {
+        jms.send(queue, ses -> ses.createTextMessage(text));
+    }
+}
---
+++ pass-core/pass-core-main/src/test/java/org/eclipse/pass/main/JmsConfigurationTest.java
@@ -0,0 +1,181 @@
+package org.eclipse.pass.main;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.net.URI;
+import java.util.Collections;
+import java.util.Enumeration;
+import javax.jms.JMSException;
+import javax.jms.QueueBrowser;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.stream.JsonParser;
+import javax.json.stream.JsonParser.Event;
+
+import com.yahoo.elide.RefreshableElide;
+import org.eclipse.pass.object.ElidePassClient;
+import org.eclipse.pass.object.PassClient;
+import org.eclipse.pass.object.model.Deposit;
+import org.eclipse.pass.object.model.DepositStatus;
+import org.eclipse.pass.object.model.EventType;
+import org.eclipse.pass.object.model.Submission;
+import org.eclipse.pass.object.model.SubmissionEvent;
+import org.eclipse.pass.object.model.SubmissionStatus;
+import org.eclipse.pass.usertoken.BadTokenException;
+import org.eclipse.pass.usertoken.Token;
+import org.eclipse.pass.usertoken.TokenFactory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.BrowserCallback;
+import org.springframework.jms.core.JmsTemplate;
+
+public class JmsConfigurationTest extends IntegrationTest {
+    @Autowired
+    private JmsTemplate jms;
+
+    @Autowired
+    protected RefreshableElide refreshableElide;
+
+    @Autowired
+    private JmsConfiguration jmsConfig;
+
+    @Autowired
+    private TokenFactory userTokenFactory;
+
+    private PassClient client;
+
+    private void clear_queue(String queue) throws JMSException {
+        int count = count_queue(queue);
+
+        while (count-- > 0) {
+            jms.receive(queue).acknowledge();
+        }
+    }
+
+    private int count_queue(String queue) {
+        return jms.browse(queue, new BrowserCallback<Integer>() {
+            @Override
+            public Integer doInJms(final Session session, final QueueBrowser browser) throws JMSException {
+                return Collections.list((Enumeration<?>)browser.getEnumeration()).size();
+            }
+        });
+    }
+
+    @BeforeEach
+    public void setupClient() throws JMSException {
+        // Use this implementation of PassClient in order to invoke hooks
+        client = new ElidePassClient(refreshableElide);
+
+        clear_queue(jmsConfig.getSubmissionQueue());
+        clear_queue(jmsConfig.getSubmissionEventQueue());
+        clear_queue(jmsConfig.getDepositQueue());
+    }
+
+    @AfterEach
+    public void cleanupClient() throws IOException {
+        client.close();
+    }
+
+    private JsonObject parse_json(String json) {
+        try (JsonParser parser = Json.createParser(new StringReader(json))) {
+            if (parser.hasNext() && parser.next() == Event.START_OBJECT) {
+                return parser.getObject();
+            }
+        }
+
+        fail("Expected JSON object: " + json);
+        return null;
+    }
+
+    private JsonObject get_json_message(String queue) throws JMSException {
+        TextMessage mesg = (TextMessage) jms.receive(queue);
+        mesg.acknowledge();
+
+        return parse_json(mesg.getText());
+    }
+
+    @Test
+    public void testSubmissionMesssage() throws IOException, JMSException {
+        Submission sub = new Submission();
+        sub.setSubmitterName("Bob");
+        sub.setSubmitted(true);
+
+        client.createObject(sub);
+
+        JsonObject result = get_json_message(jmsConfig.getSubmissionQueue());
+
+        assertEquals(sub.getId().toString(), result.getString(JmsConfiguration.SUBMISSION_KEY));
+        assertEquals(JmsConfiguration.SUBMISSION_MESSAGE_TYPE, result.getString(JmsConfiguration.TYPE_KEY));
+
+        sub.setSubmissionStatus(SubmissionStatus.SUBMITTED);
+
+        client.updateObject(sub);
+
+        result = get_json_message(jmsConfig.getSubmissionQueue());
+
+        assertEquals(sub.getId().toString(), result.getString(JmsConfiguration.SUBMISSION_KEY));
+        assertEquals(JmsConfiguration.SUBMISSION_MESSAGE_TYPE, result.getString(JmsConfiguration.TYPE_KEY));
+    }
+
+    @Test
+    public void testSubmissionEventMessage() throws JMSException, IOException {
+        SubmissionEvent se = new SubmissionEvent();
+        se.setComment("This is a comment");
+
+        client.createObject(se);
+
+        JsonObject result = get_json_message(jmsConfig.getSubmissionEventQueue());
+
+        assertEquals(se.getId().toString(), result.getString(JmsConfiguration.SUBMISSION_EVENT_KEY));
+        assertEquals(JmsConfiguration.SUBMISSION_EVENT_MESSAGE_TYPE, result.getString(JmsConfiguration.TYPE_KEY));
+    }
+
+    @Test
+    public void testSubmissionEventMessageWithApprovalLink() throws JMSException, IOException, BadTokenException {
+        Submission sub = new Submission();
+        sub.setSubmitterEmail(URI.create("mailto:someone@example.com"));
+
+        client.createObject(sub);
+
+        SubmissionEvent se = new SubmissionEvent();
+        se.setSubmission(sub);
+        se.setComment("This submission needs approval from new user");
+        se.setEventType(EventType.APPROVAL_REQUESTED_NEWUSER);
+        se.setLink(URI.create("http://pass.example.com/ui"));
+
+        client.createObject(se);
+
+        JsonObject result = get_json_message(jmsConfig.getSubmissionEventQueue());
+
+        assertEquals(se.getId().toString(), result.getString(JmsConfiguration.SUBMISSION_EVENT_KEY));
+        assertEquals(JmsConfiguration.SUBMISSION_EVENT_MESSAGE_TYPE, result.getString(JmsConfiguration.TYPE_KEY));
+
+        String link = result.getString(JmsConfiguration.APPROVAL_LINK_KEY);
+
+        Token token = userTokenFactory.fromUri(URI.create(link));
+
+        assertEquals(sub.getId(), token.getPassResourceIdentifier());
+        assertEquals("submission", token.getPassResourceType());
+        assertEquals(sub.getSubmitterEmail(), token.getReference());
+    }
+
+    @Test
+    public void testDepositMessage() throws JMSException, IOException {
+        Deposit dep = new Deposit();
+        dep.setDepositStatus(DepositStatus.ACCEPTED);
+
+        client.createObject(dep);
+
+        JsonObject result = get_json_message(jmsConfig.getDepositQueue());
+
+        assertEquals(dep.getId().toString(), result.getString(JmsConfiguration.DEPOSIT_KEY));
+        assertEquals(JmsConfiguration.DEPOSIT_MESSAGE_TYPE, result.getString(JmsConfiguration.TYPE_KEY));
+    }
+}
