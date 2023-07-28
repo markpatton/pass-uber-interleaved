b34222467474d395353930a2b0bfe474b54cfa6d
Timothy Sanders <tsande16@jhu.edu>
2023-07-28 11:30:52 -0400

    Add grantAwardNumberNormalizer

--- pass-support/pass-data-client/src/main/java/org/eclipse/pass/support/client/Util.java
+++ pass-support/pass-data-client/src/main/java/org/eclipse/pass/support/client/Util.java
@@ -1,6 +1,11 @@
 package org.eclipse.pass.support.client;
 
+import java.io.IOException;
 import java.time.format.DateTimeFormatter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * Utilities for working with the model.
@@ -17,5 +22,30 @@ public class Util {
      */
     public static DateTimeFormatter dateTimeFormatter() {
         return FORMATTER;
+    }
+
+    /**
+     * Normalizes an award number by standardizing to the format: [A-Z0-9]{3}\s[A-Z0-9]{8}
+     * e.g. K23 HL153778
+     * @param awardNumber
+     * @return
+     */
+    public static String grantAwardNumberNormalizer(String awardNumber) throws IOException {
+        if (StringUtils.isEmpty(awardNumber)) {
+            return null;
+        }
+        if (awardNumber.matches("^[A-Z0-9]{3}\s[A-Z0-9]{8}$")) {
+            return awardNumber;
+        }
+        String regex = "[A-Z0-9]{3}\\s[A-Z0-9]{8}";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(awardNumber);
+        //if not in the format we expect, try to normalize it
+        if (matcher.find()) {
+            return matcher.group();
+        } else {
+            throw new IOException("Award number cannot be normalized: " + awardNumber);
+        }
     }
 }
