1d4ecd6f6a2da2ad4ad391be1dbf3f501fd91cf0
Russ Poetker <rpoetke1@jh.edu>
2023-08-10 15:16:03 -0400

    Fix jar file name

--- pass-support/pass-nihms-loader/nihms-docker/entrypoint.sh
+++ pass-support/pass-nihms-loader/nihms-docker/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Execute NIHMS harvest
-java -Dnihmsetl.harvester.configfile=$NIHMS_HARVEST_CONFIG -Dpass.core.url=$PASS_CORE_URL -Dpass.core.user=$PASS_CORE_USER -Dpass.core.password=$PASS_CORE_PASSWORD -jar nihms-data-harvest-exec.jar
+java -Dnihmsetl.harvester.configfile=$NIHMS_HARVEST_CONFIG -Dpass.core.url=$PASS_CORE_URL -Dpass.core.user=$PASS_CORE_USER -Dpass.core.password=$PASS_CORE_PASSWORD -jar nihms-data-harvest-cli-exec.jar
 
 # Execute NIHMS transform and load into PASS
 java -Dnihmsetl.loader.configfile=$NIHMS_LOAD_CONFIG -Dpass.core.url=$PASS_CORE_URL -Dpass.core.user=$PASS_CORE_USER -Dpass.core.password=$PASS_CORE_PASSWORD -jar nihms-data-transform-load-exec.jar
