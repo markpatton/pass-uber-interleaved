363f8ea64e68302da1b7307af58b3d11b8a83932
Russ Poetker <rpoetke1@jh.edu>
2023-08-10 11:44:30 -0400

    Change env var name

--- pass-support/pass-nihms-loader/nihms-docker/entrypoint.sh
+++ pass-support/pass-nihms-loader/nihms-docker/entrypoint.sh
@@ -4,4 +4,4 @@
 java -Dnihmsetl.harvester.configfile=$NIHMS_HARVEST_CONFIG -Dpass.core.url=$PASS_CORE_URL -Dpass.core.user=$PASS_CORE_USER -Dpass.core.password=$PASS_CORE_PASSWORD -jar nihms-data-harvest-exec.jar
 
 # Execute NIHMS transform and load into PASS
-java -Dnihmsetl.loader.configfile=$NIHMS_TRANSFORM_CONFIG -Dpass.core.url=$PASS_CORE_URL -Dpass.core.user=$PASS_CORE_USER -Dpass.core.password=$PASS_CORE_PASSWORD -jar nihms-data-transform-load-exec.jar
+java -Dnihmsetl.loader.configfile=$NIHMS_LOAD_CONFIG -Dpass.core.url=$PASS_CORE_URL -Dpass.core.user=$PASS_CORE_USER -Dpass.core.password=$PASS_CORE_PASSWORD -jar nihms-data-transform-load-exec.jar
